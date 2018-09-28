ec0ac4660650382b7a92112a723982f6b27beeb9
wuulong <wuulong@gmail.com>
2018-09-28 15:51:34 +0800

    WIP,mechanical-have position bug, receive error message content

---
+++ mechanical/robot_model_try1/README.txt
@@ -0,0 +1,1 @@
+try to make the first user define robot_model work.
---
+++ mechanical/robot_model_try1/update.sh
@@ -0,0 +1,5 @@
+cp ./turtlebot3_fake_harvest.launch ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_fake/launch/
+cp ./description_harvest.launch.xml ~/catkin_ws/src/turtlebot3/turtlebot3_bringup/launch/includes/
+cp ./turtlebot3_harvest.urdf.xacro ~/catkin_ws/src/turtlebot3/turtlebot3_description/urdf/
+cp ./cyliner.stl ~/catkin_ws/src/turtlebot3/turtlebot3_description/meshes/bases/
+cp ./semi_sphere.stl ~/catkin_ws/src/turtlebot3/turtlebot3_description/meshes/bases/
