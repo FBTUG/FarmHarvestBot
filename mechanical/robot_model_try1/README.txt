3e4be0e9682a5090932c43f261f87402aa8c4b4b
wuulong <wuulong@gmail.com>
2018-09-30 11:55:52 +0800

    WIP, mechanical-robot model move!

--- mechanical/robot_model_try1/README.txt
+++ mechanical/robot_model_try1/README.txt
@@ -1,1 +1,5 @@
 try to make the first user define robot_model work.
+please reference doc: https://paper.dropbox.com/doc/FBTUG-FarmHarvestBot-Rviz--AOByR~gFMijxtPB6flxyfWHSAg-BRfzoSOSrbdxAyRyBBzzJ
+
+./update.sh
+rosrun turtlebot3_harvest turtlebot3_harvest.py
--- mechanical/robot_model_try1/update.sh
+++ mechanical/robot_model_try1/update.sh
@@ -1,5 +1,6 @@
-cp ./turtlebot3_fake_harvest.launch ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_fake/launch/
+#cp ./turtlebot3_fake_harvest.launch ~/catkin_ws/src/turtlebot3_simulations/turtlebot3_fake/launch/
 cp ./description_harvest.launch.xml ~/catkin_ws/src/turtlebot3/turtlebot3_bringup/launch/includes/
 cp ./turtlebot3_harvest.urdf.xacro ~/catkin_ws/src/turtlebot3/turtlebot3_description/urdf/
 cp ./cyliner.stl ~/catkin_ws/src/turtlebot3/turtlebot3_description/meshes/bases/
 cp ./semi_sphere.stl ~/catkin_ws/src/turtlebot3/turtlebot3_description/meshes/bases/
+cp -r ./turtlebot3_harvest ~/catkin_ws/src/turtlebot3_simulations
